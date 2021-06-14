0acca2605c52905159fb10acf097894400f000b6
Okomiya <alexandredsamson@gmail.com>
2021-06-14 20:44:06 +0200

    test

--- challenges/debutant/PIERRE_FEUILLE_CISEAUX/methode_2.py
+++ challenges/debutant/PIERRE_FEUILLE_CISEAUX/methode_2.py
@@ -4,14 +4,14 @@ def dictionnaire(data) :
     coups = data['coups']
     reponse = ""
 
-    #Dictionnaire associatif donnant les coups à jouer selon ceux donnés
+    # Dictionnaire associatif donnant les coups à jouer selon ceux donnés
     associations = {
         "P": "F",
         "F": "C",
         "C": "P"
     }
 
-    #Boucle 'for' en utilisant le dicionnaire
+    # Boucle 'for' en utilisant le dicionnaire
     for letter in coups:
         reponse += associations[letter]
 
